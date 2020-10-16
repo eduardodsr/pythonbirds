ec975113a70d3786aa2d6cde19d1f1ab261561eb
Eduardo da Silva Rodrigues <eduardodsr@gmail.com>
2020-10-15 23:46:27 -0300

    Criado e removido atributo dinâmico de objetos do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -18,7 +18,10 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
-
+    luciano.sobrenome = 'Ramalho'
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(eduardo.__dict__)
 
 
 
