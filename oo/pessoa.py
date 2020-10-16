4da47e0452ee9989bc3f0f701807bf8ec9183a8e
Eduardo da Silva Rodrigues <eduardodsr@gmail.com>
2020-10-16 00:13:10 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2 # atributo default (ou atributo de classe)
+
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -20,9 +22,11 @@ if __name__ == '__main__':
         print(filho.nome)
     luciano.sobrenome = 'Ramalho' # criação do atributo dinâmico
     del luciano.filhos # remover o atributo filhos
+    luciano.olhos = 1
+    del luciano.olhos
     print(luciano.__dict__) # atributos de instância __dict__ de um objeto
     print(eduardo.__dict__)
-
-
-
-
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(eduardo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(eduardo.olhos))
